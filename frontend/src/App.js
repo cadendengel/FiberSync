b0447e11e760bd6f522e329e8390e821afe9b73c
Chris Clark <csc121@txstate.edu>
2025-02-11 15:28:10 -0600

    Deployed via Firebase
    Added some additional elements and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import logo from './FiberSyncLogo.png'; // Make sure the logo image is in the src folder
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="FiberSync Logo" className="logo" />
       <h1>FiberSync</h1>
     </div>
   );

21c343cae689eefa5a782ada188792e4a25ba2d0
Chris Clark <csc121@txstate.edu>
2025-02-11 23:51:41 -0600

    SCRUM-12: Designing Chat Entry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './FiberSyncLogo.png'; // Make sure the logo image is in the src folder
 
 function App() {
+  const [enteredChat, setEnteredChat] = useState(false);
+
+  const handleClick = () => {
+    setEnteredChat(true);
+  };
+
   return (
-    <div className="App">
-      <img src={logo} alt="FiberSync Logo" className="logo" />
-      <h1>FiberSync</h1>
+    <div className="container">
+      {!enteredChat ? (
+        <div className="entry-box" onClick={handleClick}>
+          <img src={logo} alt="FiberSync Logo" className="logo" />
+          <h1>FiberSync</h1>
+        </div>
+      ) : (
+        <div className="chat-container">
+          <h2>Welcome to the Chat</h2>
+        </div>
+      )}
     </div>
   );
 }

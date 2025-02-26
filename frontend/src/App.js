efaf4b365a911d0a12e455ced033cfb12f21b4a9
cadendengel <oaa54@txstate.edu>
2025-02-25 22:06:48 -0600

    SCRUM-27 Changed axios API call routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
     if (enteredChat) {
-      axios.get("http://localhost:5000/api/messages/all")
+      axios.get("http://127.0.0.1:5000/api/messages/all")
         .then((response) => setMessages(response.data)) // Set messages to the response data
         .catch((error) => console.error("Error fetching messages:", error));
     }
@@ -24,7 +24,7 @@ function App() {
 
   // Passwords NYI
   const handleLogin = () => {
-    axios.post("http://localhost:5000/api/users/create", { username: username })
+    axios.post("http://127.0.0.1:5000/api/users/create", { username: username })
     .then((response) => {
       setEnteredChat(true);
       console.log("User created:", response.data); // Debugging log
@@ -36,7 +36,7 @@ function App() {
 
   // Send message to the backend
   const handleSendMessage = (chatEvent) => {
-    axios.post("http://localhost:5000/api/messages/create", new ChatMessage(username, chatEvent))
+    axios.post("http://127.0.0.1:5000/api/messages/create", new ChatMessage(username, chatEvent))
     .then((response) => {
       console.log("Message sent to backend", response.data); // debug log
       setMessages((prevMessages) => [...prevMessages, response.data]); // Append new message
@@ -46,7 +46,7 @@ function App() {
 
   // Delete message by ID (Button NYI)
   const handleDeleteMessage = (messageId) => {
-    axios.delete("http://localhost:5000/api/messages/id", messageId)
+    axios.delete("http://127.0.0.1:5000/api/messages/id", messageId)
     .then((response) => {
       console.log("Message deleted:", response.data); // Debugging log
       setMessages((prevMessages) => prevMessages.filter(message => message.id !== messageId)); // Remove deleted message

3fb752a21821414a09486dec63d54125249c792f
cadendengel <oaa54@txstate.edu>
2025-02-25 22:25:24 -0600

    SCRUM-28 / (SCRUM-33) -> Corrected user_status test import and naming

--- backend/__tests__/user_status_test.py
+++ backend/__tests__/user_status_test.py
@@ -1,7 +1,7 @@
 import unittest
 import warnings
 from datetime import datetime, timedelta
-from backend.src.UserDB.user_status import get_user_status_by_username
+from backend.src.UserDB import user_status
 
 # Test using: cd backend
 # then use this command in the terminal: PYTHONPATH=src python -m unittest discover -s backend/tests -p "test_user_status.py"
@@ -10,7 +10,7 @@ class TestUserStatusManager(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up a test database connection."""
-        cls.manager = UserStatusManager(db_name="test_fibersync")  # Use a test database
+        cls.manager = user_status.UserStatusManager(db_name="test_fibersync")  # Use a test database
         warnings.filterwarnings("ignore", category=DeprecationWarning)  # Suppress deprecation warnings
         warnings.filterwarnings("ignore", category=ResourceWarning)  # Suppress resource warnings
 

8dd6eed82c9d190c61748ceeefc6e4b3eab14a57
Caden Dengel <oaa54@txstate.edu>
2025-04-03 12:50:31 -0500

    SCRUM-38 Amended msgDB_test.py
    add_channel() required an additional channel argument

--- backend/__tests__/msgDB_test.py
+++ backend/__tests__/msgDB_test.py
@@ -21,16 +21,17 @@ class TestMsgDB(unittest.IsolatedAsyncioTestCase):
         msgDB.delete_all_messages()
 
     def test_add_message(self):
-        msgDB.add_message("test_message_id", "test_timestamp", "test_user", "test_text")
+        msgDB.add_message("test_message_id", "test_timestamp", "test_user", "test_text", "test_channel")
         message = msgDB.get_message_by_id("test_message_id")
         self.assertEqual(message["messageid"], "test_message_id")
         self.assertEqual(message["timestamp"], "test_timestamp")
         self.assertEqual(message["user"], "test_user")
         self.assertEqual(message["text"], "test_text")
+        self.assertEqual(message["channel"], "test_channel")
 
     def test_get_all_messages(self):
-        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1")
-        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2")
+        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1", "test_channel1")
+        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2", "test_channel2")
         messages = msgDB.get_all_messages()
         self.assertEqual(msgDB.get_message_count(), 2)
         self.assertEqual(messages[0]["messageid"], "test_message_id1")
@@ -41,17 +42,19 @@ class TestMsgDB(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(messages[1]["user"], "test_user2")
         self.assertEqual(messages[0]["text"], "test_text1")
         self.assertEqual(messages[1]["text"], "test_text2")
+        self.assertEqual(messages[0]["channel"], "test_channel1")
+        self.assertEqual(messages[1]["channel"], "test_channel2")
 
     def test_delete_all_messages(self):
-        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1")
-        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2")
+        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1", "test_channel1")
+        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2", "test_channel2")
         msgDB.delete_all_messages()
         messages = msgDB.get_all_messages()
         self.assertEqual(msgDB.get_message_count(), 0)
 
     def test_delete_message(self):
-        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1")
-        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2")
+        msgDB.add_message("test_message_id1", "test_timestamp1", "test_user1", "test_text1", "test_channel1")
+        msgDB.add_message("test_message_id2", "test_timestamp2", "test_user2", "test_text2", "test_channel2")
         msgDB.delete_message("test_message_id1")
         messages = msgDB.get_all_messages()
         self.assertEqual(msgDB.get_message_count(), 1)
@@ -59,6 +62,7 @@ class TestMsgDB(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(messages[0]["timestamp"], "test_timestamp2")
         self.assertEqual(messages[0]["user"], "test_user2")
         self.assertEqual(messages[0]["text"], "test_text2")
+        self.assertEqual(messages[0]["channel"], "test_channel2")
         msgDB.delete_message("test_message_id2")
         messages = msgDB.get_all_messages()
         self.assertEqual(msgDB.get_message_count(), 0)
